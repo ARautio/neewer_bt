1a428b3555208a17dcb65d6f3506ffbaebbfe89c
Aki Rautio <aki.rautio@gmail.com>
2025-02-02 21:14:58 +0200

    Add commit and change a bit the flow

--- custom_components/neewer_bt/config_flow.py
+++ custom_components/neewer_bt/config_flow.py
@@ -1,19 +1,21 @@
 """Config flow for Neewer Bluetooth."""
 from __future__ import annotations
 
+import logging
 from typing import Any
 import voluptuous as vol
 
-from homeassistant import config_entries
-from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.components.bluetooth import (
     BluetoothServiceInfoBleak,
     async_discovered_service_info,
 )
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant import config_entries
 from homeassistant.const import CONF_ADDRESS
+from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 class NeewerBTConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Neewer Bluetooth."""
@@ -25,36 +27,9 @@ class NeewerBTConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._discovery_info: BluetoothServiceInfoBleak | None = None
         self._discovered_devices: dict[str, str] = {}
 
-    async def async_step_bluetooth(self, discovery_info: BluetoothServiceInfoBleak) -> ConfigFlowResult:
-        """Handle the bluetooth discovery step."""
-        await self.async_set_unique_id(discovery_info.address)
-        self._abort_if_unique_id_configured()
-        
-        self._discovery_info = discovery_info
-        
-        return await self.async_step_bluetooth_confirm()
-    
-
-    async def async_step_bluetooth_confirm(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Confirm discovery."""
-        assert self._discovery_info is not None
-        discovery_info = self._discovery_info
-        title = discovery_info.name
-        if user_input is not None:
-            return self.async_create_entry(title=title, data={})
-
-        self._set_confirm_only()
-        placeholders = {"name": title}
-        self.context["title_placeholders"] = placeholders
-        return self.async_show_form(
-            step_id="bluetooth_confirm", description_placeholders=placeholders
-        )
-    
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
+    ) -> FlowResult:
         """Handle the user step to pick discovered device."""
         if user_input is not None:
             address = user_input[CONF_ADDRESS]
@@ -80,4 +55,34 @@ class NeewerBTConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {vol.Required(CONF_ADDRESS): vol.In(self._discovered_devices)}
             ),
+        )
+
+    async def async_step_bluetooth(self, discovery_info: BluetoothServiceInfoBleak) -> FlowResult:
+        """Handle the bluetooth discovery step."""
+        await self.async_set_unique_id(discovery_info.address)
+        self._abort_if_unique_id_configured()
+        
+        self._discovery_info = discovery_info
+        return await self.async_step_bluetooth_confirm()
+    
+
+    async def async_step_bluetooth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm discovery."""
+        if not self._discovery_info:
+            return self.async_abort(reason="no_devices_found")
+            
+        if user_input is not None:
+            return self.async_create_entry(
+                title=self._discovery_info.name,
+                data={"address": self._discovery_info.address}
+            )
+
+        self._set_confirm_only()
+        placeholders = {"name": self._discovery_info.name}
+        self.context["title_placeholders"] = placeholders
+        return self.async_show_form(
+            step_id="bluetooth_confirm",
+            description_placeholders=placeholders,
         )
