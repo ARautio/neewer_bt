ad2871c7f989feddc2ba700ae201885749c0c7ef
Aki Rautio <aki.rautio@gmail.com>
2025-02-02 22:04:38 +0200

    Establish connect disconnect setup when updating the state

--- custom_components/neewer_bt/neewer/device.py
+++ custom_components/neewer_bt/neewer/device.py
@@ -1,5 +1,8 @@
 from typing import TYPE_CHECKING, Optional
 
+from bleak import BleakClient, BleakError
+from bleak_retry_connector import establish_connection
+
 from ..const import MODELS
 
 class NeewerBTDevice:
@@ -8,6 +11,7 @@ class NeewerBTDevice:
     def __init__(self, device, model: str):
         self.state: Optional[bool] = None
         self._device = device
+        self._client = BleakClient | None
         self._model_info = MODELS[model]
         self._char_write = self._model_info["char_write"]
 
@@ -20,8 +24,26 @@ class NeewerBTDevice:
     
     async def turn_on(self) -> None:
         """Send turn on command."""
+        if self._client is None:
+            await self._connect()
         await self._device.write_gatt(self._char_write, self._include_checksum(self._model_info["turn_on"]), False)
+        await self._disconnect()
     
     async def turn_off(self) -> None:
         """Send turn off command."""
+        if self._client is None:
+            await self._connect()
         await self._device.write_gatt(self._char_write, self._include_checksum(self._model_info["turn_off"]), False)
+        await self._disconnect()
+
+    async def _connect(self) -> None:
+        """Connect to the device."""
+        try:
+            self.client = await establish_connection(self._device)
+        except BleakError as ex:
+            raise ConnectionError from ex
+        
+    async def _disconnect(self) -> None:
+        """Disconnect from the device."""
+        await self._device.disconnect()
+        self._client = None
